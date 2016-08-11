fcf018370f23516e568f5261b65ffc048fa9ab95
Carlos Ramos <Carlos Ramos>
2016-08-11 17:48:17 -0600

    Tarea 3

---
+++ src/Ejercicios.java
@@ -0,0 +1,68 @@
+
+public class Ejercicios
+{
+	//devuelve la suma de x y y
+	static int sumar(int x, int y)
+	{
+		return -1;
+	}
+	
+	//devuelve la resta de x y y
+	static int restar(int x, int y)
+	{
+		return -1;
+	}
+	//devuelve la multiplicacion de x y y
+	static int multiplicar(int x, int y)
+	{
+		return -1;
+	}
+	
+	//devuelve el residuo de x y y
+	static int residuo(int x, int y)
+	{
+		return -1;
+	}
+	
+	//devuelve true si x es par, de lo cotrario devuelve false
+	static boolean esPar(int x)
+	{
+		return false;
+	}
+	
+	//devuelve true si x es multiplo de 3, de lo cotrario devuelve false
+	static boolean esMultiploDe3(int x)
+	{
+		return false;
+	}
+	
+	//devuelve el numero mayor (x o y)
+	static int getMayor(int x,int y)
+	{
+		return -1;
+	}
+	
+	//devuelve true si la edad es mayor o igual a 18
+	static boolean esMayorDeEdad(int edad)
+	{
+		return false;
+	}
+	
+	//devuelve true si x, y y z son pares
+	static boolean sonPares(int x, int y, int z)
+	{
+		return false;
+	}
+	
+	//devuelve el numero mayor entre x, y y z
+	static int getMayorDe3(int x, int y, int z)
+	{
+		return -1;
+	}
+	
+	public static void main(String[] args)
+	{
+		
+	}
+
+}
